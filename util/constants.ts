1318a301fe79c3576a69ee8a48c35d65d01c67e4
wangjingcun <wangjingcun@aliyun.com>
2024-11-09 19:50:38 +0800

    fix broken url
    Signed-off-by: wangjingcun <wangjingcun@aliyun.com>

--- util/constants.ts
+++ util/constants.ts
@@ -1,7 +1,7 @@
 export const GITHUB_REPO_URL = 'https://github.com/smlxl/evm.codes'
 
 // Currently active hardfork from the ones available:
-// See: https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/common/src/hardforks
+// See: https://github.com/ethereumjs/ethereumjs-monorepo/tree/stateless/packages/common/src/hardforks
 export const CURRENT_FORK = 'cancun'
 export const EOF_ENABLED_FORK = 'prague'
 export const EOF_FORK_NAME = 'EOF'
